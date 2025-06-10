66090ccebcc7f01cc8339d7ca9d40d944f30b6ab
JuanFer1996 <juan_andres_fer@hotmail.com>
2025-06-10 14:44:47 -0300

    recursivo e iterativo

---
+++ Guia9/Ejercicio1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1
+{
+    internal class Program
+    {
+       static int Fibonacci(int n)
+        {
+            if (n <= 1)
+                return n;
+            else if (n < 0)
+                return 0;
+            else
+                return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("INGRESE TERMINO DE LA SERIE DE FIBONNACI QUE DESEE");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"EL NUMERO ES: {Fibonacci(n)}");
+        }
+    }
+}
---
+++ Guia9/Ejercicio1_iterativo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1_iterativo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("INGRESE TERMINO DE LA SERIE DE FIBONNACI QUE DESEE");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int a = 0, b = 1, c = 0;
+            if (n == 0)
+            {
+                Console.WriteLine("EL NUMERO ES: 0");
+            }
+            else if (n == 1)
+            {
+                Console.WriteLine("EL NUMERO ES: 1");
+            }
+            else
+            {
+                for (int i = 2; i <= n; i++)
+                {
+                    c = a + b;
+                    a = b;
+                    b = c;
+                }
+                Console.WriteLine($"EL NUMERO ES: {c}");
+            }
+        }
+    }
+}
